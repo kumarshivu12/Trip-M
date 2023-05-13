2fbc8601811a0251b442a8a54002f4f2ef129f2e
Shreya Sharma <shreyashobha0201@gmail.com>
2023-05-14 00:36:35 +0530

    added missing port

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(require("./router/adminauth"));
 app.use(require("./router/agent"));
 app.use(require("./router/vehicle"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`shreya sharma hello ${PORT} `);
